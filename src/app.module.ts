ae61fd0fae6006bee2a6442aada9a167851b32bc
Brendson <brendson.dev@gmail.com>
2024-08-29 13:57:40 -0300

    feat: create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from '@/shared/infra/env-config/env-config.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, UsersModule],
 })
 export class AppModule {}
